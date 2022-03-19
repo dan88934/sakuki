267ccc2e6cddc96b6460757764087b0445e16c70
Daniel Hughes <dan@Daniels-MacBook-Pro.local>
2022-03-19 21:53:44 +0600

    Successfully printing input text to console when it is changed

--- front-end/sakuki/src/components/TranslationBox/index.js
+++ front-end/sakuki/src/components/TranslationBox/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react'
 import { Button } from '../ButtonElements';
 import {TranslationBoxContainer, InfoWrapper, InfoRow, Column1, Column2,
         TextWrapper, TopLine, Heading, Subtitle, JpTranslationBox, EngTranslationBox, BtnWrap, SignupLink, ImgWrap,
@@ -9,6 +9,12 @@ import {Form, TextArea} from 'semantic-ui-react'
 const InfoSection = ({lightBg, id, imgStart, topLine, lightText, 
                     headLine, darkText, description, buttonLink, buttonLabel,
                     img, alt, primary, dark, dark2}) => {
+    // const [inputText, setInputText] = useState('')
+    const [inputText, setInputText] = useState('')
+    const translateText = (e) => {
+        setInputText(e)
+        console.log(inputText)
+    }
   return (
     <>
     <TranslationBoxContainer>
@@ -18,6 +24,9 @@ const InfoSection = ({lightBg, id, imgStart, topLine, lightText,
                     <TopLine>English Input</TopLine>
                     <JpTranslationBox
                     placeholder='Type text to translate into Japanese...'
+                    // onChange={(e) => setInputText(e.target.value)}
+                    onChange={(e) => translateText(e.target.value)}
+                    // onChange={translateText}
                     />
                 </Column1>
                 <Column2>

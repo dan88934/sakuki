ed64effb3f2fc229680d79e18145e06ce18a0361
Daniel Hughes <dan@Daniels-MacBook-Pro.local>
2022-03-29 17:20:54 +0600

    Started working on backend

---
+++ back-end/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	// "net/http"
+	"github.com/gin-gonic/gin"
+	tr "github.com/snakesel/libretranslate"
+	"fmt"
+	// "errors"
+)
+
+
+
+
+//Translate function - get basic translation from API
+func getBasicTranslation(c *gin.Context) {
+	englishText := c.Param("englishText") 
+	translate := tr.New(tr.Config{
+        Url:   "https://libretranslate.com",
+        Key:   "XXX",
+    })
+
+	trtext, err := translate.Translate(englishText, "en", "ja")
+    if err == nil {
+        fmt.Println(trtext)
+    } else {
+        fmt.Println(err.Error())
+    }
+	return trtext 
+}
+
+//Filter function - replace rude gretings with appropriate words and remove 'you'
+func filterTranslation(translation string) {
+	//1. If string contains 'Oi', remove it and replace with いつもお世話になっております
+
+	//2. If string contains 'Yaaa!' remove it and replace with いつもお世話になっております
+
+	//3. If string contains 'あなた’ (You) remove it and replace with お客様
+}
+
+func translate(c *gin.Context) {
+	getBasicTranslation(c *gin.Context)
+	filterTranslation(trtext)
+}
+
+
+func main() { //Our router - send a specific route to a function
+	router := gin.Default()
+	router.POST("/translate", getBasicTranslation)
+	router.Run("localhost:8080")
+}
--- front-end/sakuki/src/components/TranslationBox/index.js
+++ front-end/sakuki/src/components/TranslationBox/index.js
@@ -4,6 +4,7 @@ import {TranslationBoxContainer, InfoWrapper, InfoRow, Column1, Column2,
         TextWrapper, TopLine, Heading, Subtitle, JpTranslationBox, EngTranslationBox, BtnWrap, SignupLink, ImgWrap,
         Img} from './TranslationBoxElements'
 import {Form, TextArea} from 'semantic-ui-react'
+import axios from 'axios'
 
 //Below dynamic vars '{}' are reading from the data file 
 const InfoSection = ({lightBg, id, imgStart, topLine, lightText, 
@@ -11,10 +12,34 @@ const InfoSection = ({lightBg, id, imgStart, topLine, lightText,
                     img, alt, primary, dark, dark2}) => {
     // const [inputText, setInputText] = useState('')
     const [inputText, setInputText] = useState('')
+    const [resultText, setResultText] = useState('')
+
+
     const translateText = (e) => {
         setInputText(e)
         console.log(inputText)
-    }
+        setResultText(inputText)
+        //Send input text to API
+
+        // let data = {
+        //     q : inputText,
+        //     source : en,
+        //     target: 
+        // }        }
+
+        // axios.post(`https://libretranslate.de/languages`, data)
+        //  .then((response) => {
+        //     // setResultText(response.data.translatedText)
+        //  })
+    } //Display loading box when API is fetchign
+
+    // useEffect(() => {
+    //     axios.get('https://libretranslate.de/languages')
+    //      .then((response) => {
+    //      console.log(response.data)
+    // })
+    // }, [])
+
   return (
     <>
     <TranslationBoxContainer>
@@ -24,15 +49,14 @@ const InfoSection = ({lightBg, id, imgStart, topLine, lightText,
                     <TopLine>English Input</TopLine>
                     <JpTranslationBox
                     placeholder='Type text to translate into Japanese...'
-                    // onChange={(e) => setInputText(e.target.value)}
                     onChange={(e) => translateText(e.target.value)}
-                    // onChange={translateText}
                     />
                 </Column1>
                 <Column2>
                     <TopLine>Japanese Output</TopLine>
                     <EngTranslationBox
                      placeholder='Output text...'
+                     value={resultText}
                     />
                 </Column2>
             </InfoRow>

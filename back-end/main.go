5868bca6181291e1ee000b550e12eea5f357dd7e
Daniel Hughes <dan@Daniels-MacBook-Pro.local>
2022-04-03 03:06:29 +0600

    Removed print statements, working on creating filterTranslation function

--- back-end/main.go
+++ back-end/main.go
@@ -16,50 +16,22 @@ type translation struct {
 
 
 //Translate function - get basic translation from API
-func getBasicTranslation(c *gin.Context) {
-
-	//1. 
-
-
-	// englishText := c.Param("englishText") 
-	// translate := tr.New(tr.Config{
-    //     Url:   "https://libretranslate.com",
-    //     Key:   "XXX",
-    // })
-
-	// trtext, err := translate.Translate(englishText, "en", "ja")
-    // if err == nil {
-    //     fmt.Println(trtext)
-    // } else {
-    //     fmt.Println(err.Error())
-    // }
-
-	fmt.Println("test")
-	// englishText := c.Param("englishText") 
+func getBasicTranslation(c *gin.Context) {	
 	var newTranslation translation
-	// englishText := ioutil.ReadAll(c.Request.Body)
-	// englishText := "replace rude gretings with appropriate words"
-	// err := json.Unmarshal(c, newTranslation)
+
 	if err := c.BindJSON(&newTranslation); err != nil {
 		return //if there is an error - this will return the status (error) as response
 	}
 	fmt.Println("English text is:", newTranslation.EnglishText)
-	// if err != nil {
-  
-    //     // if error is not nil
-    //     // print error
-    //     fmt.Println(err)
-    // }
-	// text := "Hello"
-	// fmt.Println(englishText)
+	
 	translate := tr.New(tr.Config{
         Url:   "https://translate.argosopentech.com/", //Add error handler for if one of the APIs is down
         Key:   "XXX",
     })
 
-	trtext, err := translate.Translate("I am Dan ", "en", "ja")
+	basicTranslation, err := translate.Translate("I am Dan ", "en", "ja")
     if err == nil {
-        fmt.Println(trtext)
+        fmt.Println(basicTranslation)
         fmt.Println("No error ")
 
     } else {
@@ -67,22 +39,24 @@ func getBasicTranslation(c *gin.Context) {
 		fmt.Println("Error")
     }
 
-	fmt.Println("Basic translation text is:", trtext)
+	fmt.Println("Basic translation text is:", basicTranslation)
+	// return 
 }
 
-//Filter function - replace rude gretings with appropriate words and remove 'you'
-// func filterTranslation(translation string) {
+//Filter function 
+func filterTranslation(text string) {
 	//1. If string contains 'Oi', remove it and replace with いつもお世話になっております
-
+	fmt.Println("Basic translation text is:", text)
 	//2. If string contains 'Yaaa!' remove it and replace with いつもお世話になっております
 
 	//3. If string contains 'あなた’ (You) remove it and replace with お客様
-// }
-
+}
 func translate(c *gin.Context) {
+	text := "Hello"
 	getBasicTranslation(c)
-// 	filterTranslation(translation)
+	filterTranslation(text)
   }
+
 
 
 func main() { //Our router - send a specific route to a function
@@ -90,16 +64,4 @@ func main() { //Our router - send a specific route to a function
 	router.POST("/translate", translate)
 	router.Run("localhost:8080")
 
-	// englishText := "I lived in Kyoto for 1 year. Kyoto is a beautiful city. "
-	// translate := tr.New(tr.Config{
-    //     Url:   "https://trans.zillyhuhn.com/",
-    //     Key:   "XXX",
-    // })
-
-	// trtext, err := translate.Translate(englishText, "en", "ja")
-    // if err == nil {
-    //     fmt.Println(trtext)
-    // } else {
-    //     fmt.Println(err.Error())
-    // }
 }

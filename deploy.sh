33665d4b2f59a663cd0b3736aacfa19c440e2979
Daniel Hughes <dan@Daniels-MacBook-Pro.local>
2022-06-28 01:36:01 +0700

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 # Note: This deployment file is in the home dir on the server
 cd sakuki
 git fetch --all
-git reset --hard origin/$SEMAPHORE_GIT_BRANCH
+git reset --hard origin $SEMAPHORE_GIT_BRANCH
 
 # restart web application - restart docker container
 # touch /var/www/"$(echo $APP_URL | sed 's/\./_/g')"_wsgi.py</code>

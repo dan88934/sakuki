bbcff54aaa76bef987ffbbd36f7d6395a4793c1b
Daniel Hughes <dan@Daniels-MacBook-Pro.local>
2022-07-03 01:46:55 +0700

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,17 @@
 # deploy.sh
 
-# pull updated version of branch from repo
+# Pull updated version of branch from repo
 # Note: This deployment file is in the home dir on the server
-# cd sakuki
-# git fetch --all
-# git reset --hard origin $SEMAPHORE_GIT_BRANCH
 
 cd sakuki
 git pull
-# expect "Username for 'https://github.com':"
-# send "dan88934"
-# expect "Password for 'https://dan88934@github.com':"
-# send "$gh_access_token"
-# interact
 
+# Restart Docker container
 
-# restart web application - restart docker container
-# touch /var/www/"$(echo $APP_URL | sed 's/\./_/g')"_wsgi.py</code>
-docker restart sakuki
+sudo docker stop sakuki
+
+sudo docker system prune
+
+sudo docker build -t sakuki . 
+
+sudo docker run -it --rm -p 8080:8080 sakuki 
